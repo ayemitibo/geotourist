25d40a04573ccbb20fa26bcbb8323a69e18bcca5
ayemitiboabiodun02@gmail.com <ayemitiboabiodun02@gmail.com>
2021-06-10 23:31:25 +0100

    chore : added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import BaseComponent from "@/components/global";
+
+Vue.use(BaseComponent)
 
 Vue.config.productionTip = false
 
